1d5543d401512ad2744ae4acd60c1634e49ad79d
Philipp Klein <klein.philipp@gmail.com>
2022-06-08 10:48:06 +0200

    init

---
+++ purchase.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+contract Purchase {
+    // could have some storage slot optimisation here with 2 x uint128
+    uint public value;
+    uint public confirmTime;
+    address payable public seller;
+    address payable public buyer;
+
+    enum State { Created, Locked, Release, Inactive }
+    // The state variable has a default value of the first member, `State.created`
+    State public state;
+
+    modifier condition(bool condition_) {
+        require(condition_);
+        _;
+    }
+
+    /// Only the buyer can call this function.
+    error OnlyBuyer();
+    /// Only the seller can call this function.
+    error OnlySeller();
+    /// The function cannot be called at the current state.
+    error InvalidState();
+    /// The provided value has to be even.
+    error ValueNotEven();
+    /// The non-buyer can not call this function yet.
+    error CalledTooEarlyForNonBuyer();
+
+    modifier onlyBuyer() {
+        if (msg.sender != buyer)
+            revert OnlyBuyer();
+        _;
+    }
+
+    modifier onlySeller() {
+        if (msg.sender != seller)
+            revert OnlySeller();
+        _;
+    }
+
+    modifier inState(State state_) {
+        if (state != state_)
+            revert InvalidState();
+        _;
+    }
+
+    modifier completable() {
+        if (msg.sender != buyer && block.timestamp <= confirmTime)
+            revert CalledTooEarlyForNonBuyer();
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    // Ensure that `msg.value` is an even number.
+    // Division will truncate if it is an odd number.
+    // Check via multiplication that it wasn't an odd number.
+    constructor() payable {
+        seller = payable(msg.sender);
+        value = msg.value / 2;
+        if ((2 * value) != msg.value)
+            revert ValueNotEven();
+    }
+
+    /// Abort the purchase and reclaim the ether.
+    /// Can only be called by the seller before
+    /// the contract is locked.
+    function abort()
+        external
+        onlySeller
+        inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+        // We use transfer here directly. It is
+        // reentrancy-safe, because it is the
+        // last call in this function and we
+        // already changed the state.
+        seller.transfer(address(this).balance);
+    }
+
+    /// Confirm the purchase as buyer.
+    /// Transaction has to include `2 * value` ether.
+    /// The ether will be locked until confirmReceived
+    /// is called.
+    function confirmPurchase()
+        external
+        inState(State.Created)
+        condition(msg.value == (2 * value))
+        payable
+    {
+        emit PurchaseConfirmed();
+        confirmTime = block.timestamp + 5 minutes;
+        buyer = payable(msg.sender);
+        state = State.Locked;
+    }
+
+    /// Confirm that you (the buyer) received the item.
+    /// This will release the locked ether.
+    function confirmReceived()
+        external
+        onlyBuyer
+        inState(State.Locked)
+    {
+        emit ItemReceived();
+        // It is important to change the state first because
+        // otherwise, the contracts called using `send` below
+        // can call in again here.
+        state = State.Release;
+        buyer.transfer(value);
+    }
+
+    /// This function refunds the seller, i.e.
+    /// pays back the locked funds of the seller.
+    function refundSeller()
+        external
+        onlySeller
+        inState(State.Release)
+    {
+        emit SellerRefunded();
+        // It is important to change the state first because
+        // otherwise, the contracts called using `send` below
+        // can call in again here.
+        state = State.Inactive;
+        seller.transfer(3 * value);
+    }
+
+    function completePurchase()
+        external
+        completable
+        inState(State.Locked)
+    {
+        emit ItemReceived();
+        emit SellerRefunded();
+        state = State.Inactive;
+        // as state is now inactive, we don't need to worry about reentrancy
+        // also there wouldn't be enough eth for the seller to withdraw twice     
+        seller.transfer(3 * value);
+        buyer.transfer(value);
+    }
+}
